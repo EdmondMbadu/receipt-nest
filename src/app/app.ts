5bdf19921d6b85c4e880f496ddcacdaebac6e9ce
Edmond Mbadu <mbadungoma@gmail.com>
2026-02-06 20:38:19 -0800

    focusing on making the app blazing fast for now

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,5 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
-import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -11,17 +9,4 @@ import { AuthService } from './services/auth.service';
 })
 export class App {
   protected readonly title = 'receipt-nest';
-  protected readonly auth = inject(AuthService);
-  protected readonly user = this.auth.user;
-  protected readonly displayName = computed(() => {
-    const profile = this.user();
-    if (!profile) {
-      return '';
-    }
-
-    const name = `${profile.firstName} ${profile.lastName}`.trim();
-    return name || profile.email;
-  });
-
-  currentYear = new Date().getFullYear();
 }
